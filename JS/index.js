254af8aa9cc875e3155b5b41612f3df8f9df15f6
Connor  Jones <108997973+connorjjones@users.noreply.github.com>
2022-09-02 09:34:24 -0600

    Creating About Page and adding links

--- JS/index.js
+++ JS/index.js
@@ -3,5 +3,5 @@ $("#myWorkBtn").on("click", function(){
     console.log("You Clicked on My Work")
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#myWorkSection").offset().top
-io/UX_UI_HW_20/    }, 400);
+    }, 400);
 });
